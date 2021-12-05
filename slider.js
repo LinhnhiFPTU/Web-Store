dd0d409cdea8bea84f0eb65c3caedf21d2047f28
Phuoc <PhuocPBSE161284@fpt.edu.vn>
2021-12-05 18:39:41 +0700

    Add file

---
+++ slider.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('.image-slider').slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 2,
+      autoplay: false,
+      autoplaySpeed: 1000,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name="chevron-back-outline"></ion-icon></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name="chevron-forward-outline"></ion-icon></button>`,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        },
+      ]
+    });
+  });
