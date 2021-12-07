edac3da9fc777d9279243a1c1c4667160214d077
joliebui2002@gmail.com <joliebui2002@gmail.com>
2021-12-07 14:34:11 +0800

    add files

--- content.js
+++
@@ -1,8 +0,0 @@
-const menuSlide = () => {
-    const menu = document.querySelector('.menu')
-    const nav = document.querySelector('.product-name')
-    menu.addEventListener('click', () => {
-        nav.classList.toggle('menu-active')
-    })
-}
-menuSlide();
