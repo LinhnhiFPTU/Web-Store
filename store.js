ad8c226f3f0ca838d8b5afbc24528a17f6d49777
joliebui2002@gmail.com <joliebui2002@gmail.com>
2021-12-18 21:28:32 +0800

    add files

--- store.js
+++ store.js
@@ -1,72 +1,3 @@
-/*--------------------User-------------------------*/
-var usersAPI = "https://61b32a86af5ff70017ca1d02.mockapi.io/users";
-var logOut = document.querySelector("#logOut");
-var saveBtn = document.querySelector(".save");
-fetch(usersAPI)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(productList) {
-        handleStore(productList);
-    });
-function handleStore(productList) {
-    checkLogin();
-    logOut.onclick = function() {
-        delete localStorage.id;
-        window.location.assign("login.html");
-    };
-    handleProfile(productList);
-}
-function checkLogin() {
-  if (!window.localStorage.id) {
-    window.location.assign("login.html");
-  }
-};
-function activeBtn() {
-    document.querySelector(".save").disabled = false;
-    document.querySelector(".save").style.background = 'rgba(32, 145, 30, 1)';
-};
-function handleProfile(productList) {
-    document.querySelector(".save").disabled = true;
-    document.querySelector(".save").style.background = 'rgba(32, 145, 30, 0.5)';
-    var name = document.querySelector("#name");
-    var phone = document.querySelector("#phone");
-    var address = document.querySelector("#address");
-    var email = document.querySelector("#email");
-    for (const user of productList) {
-        if (user.id == localStorage.id){
-            name.value = `${user.fullname}`;
-            phone.value = `${user.phonenumber}`;
-            if(address.value != undefined) address.value = `${user.address}`;
-            if(email.value != undefined) email.value = `${user.email}`;
-        }
-    }
-} 
-function updataProfile(){
-    var name = document.querySelector("#name").value;
-    var phone = document.querySelector("#phone").value;
-    var address = document.querySelector("#address").value;
-    var email = document.querySelector("#email").value;
-    var data = {
-        fullname: name,
-        phonenumber: phone,
-        address: address,
-        email: email
-    };
-    fetch(usersAPI+ '/' + localStorage.id,{
-        method: 'PUT',
-        headers:{
-        'Content-Type':'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response=>{
-        return response.json()
-    })
-    .then(data=> 
-        alert("Successfully updated!"))
-}   
-
 /*--------------------Add to cart-------------------------*/
 const carts = document.querySelectorAll('.image-item');
 function addCart(productImg, productName, productPrice) {
@@ -92,7 +23,7 @@ function addCart(productImg, productName, productPrice) {
     for (var i=0; i<cartItem.length; i++) {
         var productT = document.querySelectorAll('.product-title')
         if(productT[i].innerHTML == productName) {
-            alert('Your product is already in cart')
+
         }
     }
     addtr.innerHTML = `
