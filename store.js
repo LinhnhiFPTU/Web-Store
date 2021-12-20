fc011dc91a7f95709303de7f10c6e30ed7ab3451
joliebui2002@gmail.com <joliebui2002@gmail.com>
2021-12-20 22:59:10 +0800

    add files

--- store.js
+++ store.js
@@ -24,32 +24,49 @@ function checkLogin() {
   };
 /*--------------------Add to cart-------------------------*/
 const carts = document.querySelectorAll('.image-item');
-function addCart(productImg, productName, productPrice) {
-    var addtr = document.createElement("tr")
-    // console.log(addtr)
-    var trContent = productImg
-    var cartTable = document.querySelector(".subbody")
-    console.log(cartTable)
-}
-carts.forEach(function(getItem, index) {
+carts.forEach(function(getItem) {
     getItem.addEventListener('click', function(event) {
         var btnItem = event.target;
         var product = btnItem.parentElement;
-        var productImg = product.querySelector('img').src
-        var productName = product.querySelector('.details').innerText
-        var productPrice = product.querySelector('.price span').innerText
-        addCart(productImg, productName, productPrice) 
+        var productImg = product.querySelector('img').src;
+        var productName = product.querySelector('.details').innerText;
+        var productPrice = product.querySelector('.price span').innerText;
+        showSuccessMessage();
+        addCart(productImg, productName, productPrice);
 })
 })
-function addCart(productImg, productName, productPrice) {
-    var addtr = document.createElement("tr")
-    var cartItem = document.querySelectorAll('tbody tr')
-    for (var i=0; i<cartItem.length; i++) {
-        var productT = document.querySelectorAll('.product-title')
-        if(productT[i].innerHTML == productName) {
-
-        }
+function toast({
+    message = '',
+    type = 'success',
+    duration = 3000
+}) {
+    const main = document.getElementById('alert');
+    if (main) {
+        const toast = document.createElement('div');
+        toast.style.animation = `slideInLeft ease.3s, fadeOut linear 1s 1s forwards`;
+        toast.classList.add('toast',`toast--${type}`);
+        toast.innerHTML = `
+        <div class="toast toast-success">
+        <span class="fas fa-check-circle"></span>
+        <span class="message">${message}</span>
+        <span class="fas fa-times"></span>
+        </div>
+        `;
+        main.appendChild(toast)
+        setTimeout(function() {
+            main.removeChild(toast);
+        }, duration +1000)
     }
+}
+function showSuccessMessage() {
+    toast({
+        message: 'Successfully added to cart',
+        type: 'success',
+        duration: 1000,
+      });
+}
+function addCart(productImg, productName, productPrice) {
+    var addtr = document.createElement("tr");
     addtr.innerHTML = `
     <tr>
         <td class="select-image"><img src="${productImg}"></td>
@@ -61,28 +78,28 @@ function addCart(productImg, productName, productPrice) {
             <input type="number" min="1" value="1">                             
         </td>
         <td class="select-delete"><img class="delete-icon" src="image/delete-icon.png"></td>
-        
+        <td class="total-price"><p><span>${productPrice}</span> VND</p></td>
     </tr>
     `
-    var cartTable = document.querySelector('tbody')
-    cartTable.append(addtr)
-
-    cartTotal()
-    deleteCart()
+    var cartTable = document.querySelector('tbody');
+    cartTable.append(addtr);
+    cartTotal();
+    deleteCart();
 }
-/*--------------------Total Price-------------------------*/
+/*--------------------Cart Total-------------------------*/
 function cartTotal() {
-    var cartItem = document.querySelectorAll('tbody tr')
-    var totalB = 0
+    var cartItem = document.querySelectorAll('tbody tr');
+    var totalB = 0;
     for (var i=0; i<cartItem.length; i++) {
-        var inputValue = cartItem[i].querySelector('input').value
-        var productPrice = cartItem[i].querySelector('.select-price span').innerHTML
-        var totalA = inputValue*productPrice*1000
-        totalB = totalB + totalA
+        var inputValue = cartItem[i].querySelector('input').value;
+        var productPrice = cartItem[i].querySelector('.select-price span').innerHTML;
+        var totalA = inputValue*productPrice*1000;
+        totalB = totalB + totalA;
     }
-    var cartTotal = document.querySelector('.price-total span')
-    cartTotal.innerHTML = totalB.toLocaleString('de-DE')
-    inputChange()
+    var cartTotal = document.querySelector('.cart-total span')
+    cartTotal.innerHTML = totalB.toLocaleString('de-DE');
+    inputChange();
+    success()
 }
 function inputChange() {
     var cartItem = document.querySelectorAll('tbody tr')
@@ -90,6 +107,7 @@ function inputChange() {
         var inputValue = cartItem[i].querySelector('input')
         inputValue.addEventListener('change', function() {
             cartTotal()
+            totalPrice()
         })
     }
 }
@@ -105,4 +123,15 @@ function deleteCart() {
             cartTotal()
         })
     }
+}
+function success() {
+    var checkOut = document.querySelector('.checkout');
+    checkOut.addEventListener('click', function() {
+        swal({
+            title: "Thank You",
+            text: "Your order has been successfully completed!",
+            icon: "success",
+            button: "OK",
+          });
+    })
 }
